032e2ac6cc068005b8f9023de4ae59f44cfb203b
Johann <johanson88@gmail.com>
2019-11-01 15:10:39 +0100

    game object created

--- src/game.js
+++ src/game.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function Game () {
+    this.canvas = null;
+    this.ctx = null;
+    this.lifes = 5;
+    this.platform= null;
+    this.points = 0;
+    this.gameIsOver = false;
+    this.ball = null;
+    this.gameScreen = null;
+}
