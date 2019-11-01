e2f170333944006dcf55b518f0117d88d0ae94b9
Johann <johanson88@gmail.com>
2019-11-01 16:30:45 +0100

    Platform handleScreenCollision + removeLife + draw

--- src/platform.js
+++ src/platform.js
@@ -4,11 +4,47 @@ function Platform (canvas, lives) {
     this.ctx = this.canvas.getContext('2d');
     this.x = 455; //half of totalCanvasSize - half of platform size;
     this.y = 420;
+    this.speed = 5;
     this.lives = 5;
     this.points = 0;
-    this.size = 90;
+    this.width = 90;
+    this.height = 15;
     this.sectionSize = 30; //size divided by 3 so 3 sections
     this.color = 'red';
     this.initPos = {x: 455, y: 420};
 }
 
+// setDirection()
+
+Platform.prototype.setDirection = function(direction) {
+    // +1 down  -1 up
+    if (direction === 'left') this.direction = -1;
+    else if (direction === 'right') this.direction = 1;
+  };
+
+Platform.prototype.handleScreenCollision = function() {
+this.x = this.x + this.direction * this.speed;
+var screenLeftBorder = 0;
+var screenRightBorder = this.canvas.width;
+
+if (this.x > screenRightBorder) this.x = screenRightBorder - this.width;
+//if it reaches the border it will not escape the screen
+else if (this.x < screenLeftBorder) this.x = 0;
+};
+
+// removeLife()
+
+Platform.prototype.removeLife = function() {
+    this.lives -= 1;
+};
+
+Platform.prototype.draw = function() {
+    this.ctx.fillStyle = this.color;
+    // fillRect(x, y, width, height)
+    this.ctx.fillRect(
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  };
