98d94b0e62430ce5e7642c0e87604f5c99e9b8dc
Johann <johanson88@gmail.com>
2019-11-01 13:28:33 +0100

    Splash + grame screen

--- src/main.js
+++ src/main.js
@@ -1,1 +1,102 @@
 'use strict';
+function htmlElementGenerator(elementTag,innerCode,elementClass,elementId) {
+    if (typeof elementTag === 'string' && elementTag.length >0) {
+        var element = document.createElement(elementTag);
+        if (typeof elementClass === 'string' && elementClass.length > 0) {
+            element.setAttribute('class', elementClass);
+        }
+        if (typeof elementId === 'string' && elementId.length > 0) {
+            element.setAttribute('id',elementId);
+        }
+        element.innerHTML = innerCode;
+        return element;
+    }else console.log('Element tag not valid');
+}
+// Runs on initial start and contains calls all other functions that manage the game
+function main() {
+  var game; // instance of the Game
+  var splashScreen; // Start Screen
+  var gameOverScreen;
+
+    
+  // -- splash screen
+
+  function buildSplashScreen() {
+    splashScreen = htmlElementGenerator('main','','splash-screen-container');
+    
+    var title = htmlElementGenerator('h1','IronBall');
+    var newGame = htmlElementGenerator('button','NEW GAME');
+    var topScores = htmlElementGenerator('button', 'TOP SCORES');
+    splashScreen.appendChild(title);
+    splashScreen.appendChild(newGame);
+    splashScreen.appendChild(topScores);
+
+    newGame.addEventListener('click', function() {
+        removeSplashScreen();
+        buildGameScreen();
+
+      });
+    
+
+
+    document.body.appendChild(splashScreen);
+  }
+
+  function removeSplashScreen() {
+      //Tested!
+      splashScreen.remove();
+  }
+
+    
+  function buildGameScreen() {
+      
+      var gameContainer = htmlElementGenerator('main','');
+      var canvasContainer = htmlElementGenerator('div','','canvas-container');
+      var canvas = htmlElementGenerator('canvas');
+      canvasContainer.appendChild(canvas);
+
+      var gameInfoContainer = htmlElementGenerator('section','','game-info-container');
+      var lifeContainer = htmlElementGenerator('div', '','lifes');
+      lifeContainer.appendChild(htmlElementGenerator('span','Lifes: ','label'));
+      lifeContainer.appendChild(htmlElementGenerator('span','','value'));
+      
+      var timeContainer = htmlElementGenerator('div','','time');
+      timeContainer.appendChild(htmlElementGenerator('span','Time: ','label'));
+      timeContainer.appendChild(htmlElementGenerator('span','','value'));
+
+      var pointsContainer = htmlElementGenerator('div', '', 'points');
+      pointsContainer.appendChild(htmlElementGenerator('span','Points: ','label'));
+      pointsContainer.appendChild(htmlElementGenerator('span','','value'));
+      gameInfoContainer.appendChild(lifeContainer);
+      gameInfoContainer.appendChild(timeContainer);
+      gameInfoContainer.appendChild(pointsContainer);
+
+      gameContainer.appendChild(canvasContainer);
+      gameContainer.appendChild(gameInfoContainer);
+      
+      document.body.appendChild(gameContainer);
+  }
+
+  function removeGameScreen() {}
+
+    
+  // -- game over screen & restart button
+
+  function buildGameOverMessage(score) {}
+
+  function removeGameOverMessage() {}
+
+    
+  // -- Setting the game state 
+
+  function startGame() {}
+
+  function gameOver() {}
+
+    
+  // -- initialize Splash screen on initial start
+  buildSplashScreen();
+}
+
+// Runs the function `main` once all resources are loaded
+window.addEventListener('load', main);
