7bf4650de5887514c875d967045175a35598c459
Johann <johanson88@gmail.com>
2019-11-11 17:02:43 +0100

    class scores ->ES6

--- src/scores.js
+++ src/scores.js
@@ -1,36 +1,37 @@
 'use strict';
-function Scores () {
-    this.topScores = []; //array of objets {name, score}
+class Scores {
+    constructor() {
+        this.topScores = []; //array of objets {name, score}
+    }
+    addNewScore (nameScoreObj) {
+        this.topScores.push(nameScoreObj);
+        this.sortScores();
+    }
+    
+    sortScores() {
+        this.topScores.sort(function (a,b) {
+            if (a.score > b.score) return -1;
+            else if (a.score < b.score) return 1;
+            else return 0;
+        });
+    }
+    
+    getScores () {
+    
+        return this.topScores.filter(function(element,index) {
+            if (index < 9) return true;
+        });
+    }
+    
+    saveToLocalStorage () {
+        localStorage.clear();
+        var scoreStringified = JSON.stringify(this.topScores);
+        localStorage.setItem('score', scoreStringified);
+    }
+    loadFromLocalStorage () {
+        if (localStorage.getItem('score')) {
+            var retrieved = localStorage.getItem('score');
+            this.topScores = JSON.parse(retrieved);
+        }    
+    }  
 };
-
-Scores.prototype.addNewScore = function (nameScoreObj) {
-    this.topScores.push(nameScoreObj);
-    this.sortScores();
-}
-
-Scores.prototype.sortScores = function() {
-    this.topScores.sort(function (a,b) {
-        if (a.score > b.score) return -1;
-        else if (a.score < b.score) return 1;
-        else return 0;
-    });
-}
-
-Scores.prototype.getScores = function () {
-
-    return this.topScores.filter(function(element,index) {
-        if (index < 9) return true;
-    });
-}
-
-Scores.prototype.saveToLocalStorage = function () {
-    localStorage.clear();
-    var scoreStringified = JSON.stringify(this.topScores);
-    localStorage.setItem('score', scoreStringified);
-}
-Scores.prototype.loadFromLocalStorage = function () {
-    if (localStorage.getItem('score')) {
-        var retrieved = localStorage.getItem('score');
-        this.topScores = JSON.parse(retrieved);
-    }    
-}
